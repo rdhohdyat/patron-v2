2330bcbb739db761fa80ad71d9ddf3b7b236f48b
ievan <ievan22ti@mahasiswa.pcr.ac.id>
2024-07-20 11:12:58 +0700

    menambahkan fungsi controller pada order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Store;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
@@ -47,6 +48,10 @@ class OrderController extends Controller
         Order::create($data);
 
         return to_route('shop');
+    }
+    public function storeCart(Request $request)
+    {
+        dd($request);
     }
 
     /**
